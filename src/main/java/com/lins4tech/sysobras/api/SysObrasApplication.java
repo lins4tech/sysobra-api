73f4001f6c30336962642a9c84cef1440e016142
Leurimar Lins <leurimar.lins@gmail.com>
2018-11-07 16:49:40 -0300

    Commit Inicial do SysObras API

---
+++ src/main/java/com/lins4tech/sysobras/api/SysObrasApplication.java
@@ -0,0 +1,12 @@
+package com.lins4tech.sysobras.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysObrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysObrasApplication.class, args);
+	}
+}
